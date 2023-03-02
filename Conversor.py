df593f531568310b207593c65a35236c87d99e90
Isram73 <124229203+Isram73@users.noreply.github.com>
2023-03-02 13:11:01 -0600

    Add files via upload

---
+++ Conversor.py
@@ -0,0 +1,17 @@
+#App para convertir pies a metros usando Tkinter
+#Crhistopher Isram Mancilla Laure
+#23 de Febrero del 2023
+
+from tkinter import* #Esta es una forma de importar paquetes de Python 
+from tkinter import ttk 
+
+class Conversor:
+
+    #Tipo constructor de la clase
+    def __init__(self, raiz):
+        raiz.title("Pies a metros")
+
+if __name__=="__main__":
+    print("Este es el archivo principal")
+    print("Nada más se mostrará esto si es el principal")
+
