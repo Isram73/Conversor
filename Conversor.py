2a5e6fb1384fb809de51c1c443677a57f1e07ea7
Isram73 <124229203+Isram73@users.noreply.github.com>
2023-03-07 21:11:43 -0600

    Add files via upload

--- Conversor.py
+++ Conversor.py
@@ -14,19 +14,21 @@ class Conversor:
         self.pies = StringVar()
         self.metros = StringVar()
 
-        mainFrame = ttk.Frame(raiz)
+        mainFrame = ttk.Frame(raiz, padding = "10 10 10 10") #Padding(Izquierda, arriba, derecha, abajo)
         mainFrame.grid(column=0, row=0) 
 
-        piesEntry = ttk.Entry(mainFrame, textvariable=self.pies) #Caja de texto
+        piesEntry = ttk.Entry(mainFrame, width=7, textvariable=self.pies) #Caja de texto
         piesEntry.grid(column=1, row=0)
 
-        ttk.Label(mainFrame, text="Pies").grid(column=2, row=0)
-        ttk.Label(mainFrame, text="Son equivalentes a:").grid(column=0, row=1)
+        ttk.Label(mainFrame, text="Pies").grid(column=2, row=0, sticky=(W, E, N, S))
+        #W lo pega a la izquierda, E a la derecha, N arriba y la S abajo
+        ttk.Label(mainFrame, text="Son equivalentes a:", padding=(5, 10, 5, 10)).grid(column=0, row=1)
         ttk.Label(mainFrame, textvariable=self.metros).grid(column=1, row=1)
         ttk.Label(mainFrame, text="Metros").grid(column=2, row=1)
 
         ttk.Button(mainFrame, text="Calcular", command=self.calcular).grid(column=2, row=2) #Botón
 
+        piesEntry.focus() #Esto hace que en cuando entremos a la ventana ya estemos listos para escribir
         #Hacer la operación presionando <Enter>
         raiz.bind("<Return>", self.calcular)
     
@@ -34,10 +36,14 @@ class Conversor:
         print("Botón presionar")
         piesUsuario = self.pies.get() #Siempre devuelve una cadena
         print("Pies ingresados: ", piesUsuario)
-        piesFlotante = float(piesUsuario) #Conversión de cadena a flotante
-        metros = piesFlotante * 0.3048
-        print("Metros: ", metros)
-        self.metros.set(metros)
+        try:       
+            piesFlotante = float(piesUsuario) #Conversión de cadena a flotante
+            metros = piesFlotante * 0.3048
+            print("Metros: ", metros)
+            self.metros.set(metros)
+            #self.pies.set("") #Método para limpiar el Entry después de Calcular
+        except:
+            print("No es un dato válido.")    
        
 
 if __name__=="__main__":
